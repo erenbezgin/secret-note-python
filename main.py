f7915d3f38b1ab76b63df13591d12933c45e16ee
Eren BEZGİN <erenbezgin2@gmail.com>
2026-05-20 15:53:38 +0300

    Vigenere şifrelemesi ve dosya kaydetme mantığı eklendi.

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
 from tkinter import *
+from tkinter import messagebox
+import base64
 
-FONT = ("Verdana", 10, "italic")
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
+def kayıtvesifre():
+    baslık = baslıkkutu.get()
+    mesaj = notgir.get("1.0", END)
+    sifre = sifrekutu.get()
+    if len(baslık) == 0 or len(mesaj) == 0 or len(sifre) == 0:
+        messagebox.showwarning(title="ERROR!!!", message="please enter all info")
+    else:
+        mesaj_encrypted = encode(sifre, mesaj)
+        # sifreleme ve kayıt
+        try:
+            with open("gizli.txt", "a") as data_file:
+                data_file.write(f"\n{baslık}\n{mesaj_encrypted}")
+        except:
+            with open("gizli.txt", "a") as data_file:
+                data_file.write(f"\n{baslık}\n{mesaj_encrypted}")
+        finally:
+            baslıkkutu.delete(0, END)
+            sifrekutu.delete(0, END)
+            notgir.delete("1.0", END)
+
+
+def sifrecoz():
+    sifrelenmismesaj = notgir.get("1.0", END)
+    sifre = sifrekutu.get()
+    if len(sifrelenmismesaj) == 0 or len(sifre) == 0:
+        messagebox.showwarning(title="ERROR!!!", message="please enter all info")
+    else:
+        try:
+            cozulmus_mesaj = decode(sifre, sifrelenmismesaj)
+            notgir.delete("1.0", END)
+            notgir.insert("1.0", cozulmus_mesaj)
+        except:
+            messagebox.showwarning(
+                title="ERROR!!!", message="please enter encrypted text "
+            )
+
+
+# font ve pencere
+FONT = ("Verdana", 20, "italic")
 pencere = Tk()
 pencere.title("Secret Notes")
 pencere.config(padx=30, pady=30)
-pencere.minsize(400, 700)
+pencere.minsize(400, 600)
 
 # logo
-foto = PhotoImage(file=r"C:\Users\Eren\Desktop\secret-note-python\secret.png")
+foto = PhotoImage(file="secret.png")
 foto_kucuk = foto.subsample(2, 2)
-fotobuton = Button(image=foto_kucuk)
-fotobuton
+fotobuton = Label(image=foto_kucuk)
 fotobuton.pack()
 # baslık
 baslık = Label(text="Enter your title", font=FONT)
@@ -31,9 +90,9 @@ sifre.pack()
 sifrekutu = Entry(width=30)
 sifrekutu.pack()
 # butonlar
-kayıt = Button(text="Save and Encrypt", font=FONT)
+kayıt = Button(text="Save and Encrypt", font=FONT, command=kayıtvesifre)
 kayıt.pack()
-coz = Button(text="Decrypt", font=FONT)
+coz = Button(text="Decrypt", font=FONT, command=sifrecoz)
 coz.pack()
 
 # ayakta tut
